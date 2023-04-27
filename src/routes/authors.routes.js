0d33ee9e46bf0e2528e85e286ca460449647066d
Rgiacobbo <rgiacobbofilho@gmail.com>
2023-04-27 18:25:58 -0300

    :sparkles: (CRUD) Creation of author controller

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -1,15 +1,17 @@
 const { Router } = require("express");
 
+const authorsController = require("../controllers/authors.controller");
+
 const routes = Router();
 
-routes.get("/authors");
+routes.get("/authors", authorsController.list);
 
-routes.post("/authors");
+routes.post("/authors", authorsController.create);
 
-routes.get("/authors/:id");
+routes.get("/authors/:id", authorsController.getById);
 
-routes.put("/authors/:id");
+routes.put("/authors/:id", authorsController.update);
 
-routes.delete("/authors/:id");
+routes.delete("/authors/:id", authorsController.remove);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const { PORT } = require("./config/env");
 
-const authorsRoutes = require("./routes/authors.routes");
+const authorRoutes = require("./routes/authors.routes");
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
-app.use(authorsRoutes);
+app.use(authorRoutes);
 
 app.listen(PORT, () => {
   console.log(`API Running on port ${PORT}`);
